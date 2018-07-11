ca172766e706642ad4e6dee3cbc89e27bd4aa38c
David Eads <davideads@gmail.com>
2018-07-11 09:32:33 -0700

    simple offset-based geocoder script, plus related schema updates

--- processors/geocode_addresses.py
+++ processors/geocode_addresses.py
@@ -1,22 +1,33 @@
 import geocoder
 import records
+import json
 import sys
 
 db = records.Database('postgres://localhost/iltickets')
 
-def process(offset):
+def process(limit, offset):
     rows = db.query("""
         select
-            distinct violation_location, zipcode
+            distinct address
         from parking
-        limit :offset
+        where geocode is null
+        limit :limit
         offset :offset
-    """, offset=offset)
-    import ipdb; ipdb.set_trace();
-    # geocoder.google(
-    pass
+    """, offset=offset, limit=limit)
+
+    for row in rows:
+        geocode = geocoder.google(row['address'])
+        db.query("""
+            update
+                parking
+            set
+                geocode=:geocode
+            where
+                address=:address
+        """, geocode=json.dumps(geocode.geojson), address=row['address'])
 
 
 if __name__ == '__main__':
-    offset = sys.argv[1]
-    process(offset)
+    limit = sys.argv[1]
+    offset = sys.argv[2]
+    process(limit, offset)
