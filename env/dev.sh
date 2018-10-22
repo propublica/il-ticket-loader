3ad50bad1af7548b0ad6f9d399da11ac90c9d82d
David Eads <davideads@gmail.com>
2018-10-22 11:31:10 -0500

    fix postgres dialect in config
    psycopg2 tells me I should use 'postgresql' instead of 'postgres' in connection strings; this works with psql and other command line tools it turns out

--- env/dev.sh
+++ env/dev.sh
@@ -1,4 +1,4 @@
-export ILTICKETS_DB_URL=postgres://localhost/iltickets
-export ILTICKETS_DB_ROOT_URL=postgres://localhost
+export ILTICKETS_DB_URL=postgresql://localhost/iltickets
+export ILTICKETS_DB_ROOT_URL=postgresql://localhost
 export ILTICKETS_DB_NAME=iltickets
 export ILTICKETS_DB_STRING="dbname=iltickets"
