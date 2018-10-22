bfbaa30bf81053f1b67085226f2e683af881954e
David Eads <davideads@gmail.com>
2018-10-22 11:49:56 -0500

    first db test

--- tests/test_db.py
+++ tests/test_db.py
@@ -10,14 +10,19 @@ YEARS=2018 make all
 Tests may be run against a full dataset.
 """
 import os
+import pytest
 import records
+
 
 START_DATE = '2018-01-01'
 
-db = records.Database(os.environ.get('ILTICKETS_DB_URL'))
+
+@pytest.fixture
+def db():
+    return records.Database(os.environ.get('ILTICKETS_DB_URL'))
 
 
-def test_record_length():
+def test_record_length(db):
     results = db.query("""
         select
             count(*) as count
@@ -29,4 +34,5 @@ def test_record_length():
 
     # The number of rows imported should be the number of lines in the source file,
     # minus 2 for the header and total rows included in the files.
-    assert results.first().get('count') == 769219
+    num_rows = results.first().get('count')
+    assert num_rows == 769219
