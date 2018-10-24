3c73e257f436c0d91423d252e5185b38ed6cb909
David Eads <davideads@gmail.com>
2018-10-24 10:29:00 -0500

    join through multiple addresses and lat / lngs

--- sql/views/blocksummary_yearly.sql
+++ sql/views/blocksummary_yearly.sql
@@ -9,8 +9,11 @@ as
       sum(b.fine_level1_amount) AS fine_level1_amount,
       sum(b.fine_level2_amount) AS fine_level2_amount,
       sum(b.total_payments) AS total_payments
-     FROM blocksummary_intermediate b
-       JOIN geocodes g ON b.address = g.address
+    FROM blocksummary_intermediate b
+    JOIN
+      raw_geocodes r ON b.address = r.address
+    JOIN
+      geocodes g on r.address = g.address
     WHERE g.geocoded_address is not null
     GROUP BY g.geocoded_address, b.year, b.violation_code
     ORDER BY (sum(b.ticket_count)) DESC
