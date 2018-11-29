02f117452574cb3613c67e8b84865b4cd08b71f8
David Eads <davideads@gmail.com>
2018-11-29 00:58:24 -0600

    update db schema and indexes for geocodio

--- sql/tables/geocodes.sql
+++ sql/tables/geocodes.sql
@@ -1,12 +1,13 @@
 CREATE TABLE public.geocodes (
-  id serial primary key,
   address character varying,
   geocoded_address character varying,
   geocoded_lng double precision,
   geocoded_lat double precision,
   geocoded_city character varying,
   geocoded_state character varying,
+  geocoded_zip character varying,
   geocode_accuracy character varying,
+  geocode_accuracy_type character varying,
   geocode_geojson jsonb,
   geom geometry(Geometry,4326)
 )
