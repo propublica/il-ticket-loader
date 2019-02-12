7aa10291c426fc325f3f2e624930596a47fe22dd
David Eads <davideads@gmail.com>
2019-02-11 22:29:40 -0600

    camera data now runs through python processor and needs new columns

--- sql/tables/cameras.sql
+++ sql/tables/cameras.sql
@@ -20,5 +20,12 @@ CREATE TABLE public.cameras (
   notice_level character varying,
   hearing_disposition character varying,
   notice_number bigint,
-  officer character varying
+  dismissal_reason character varying(128),
+  officer character varying,
+  address character varying(80),
+  license_hash character varying,
+  year int,
+  month int,
+  hour int,
+  penalty double precision null
 );
