ce143111a7ef7637f8b4a9185ad877f6aac8562c
David Eads <davideads@gmail.com>
2019-04-09 00:14:22 -0500

    join census geography

--- sql/indexes/parking.sql
+++ sql/indexes/parking.sql
@@ -1,3 +1,7 @@
 create index if not exists parking_cluster on parking (address, violation_code, year);
 create index if not exists parking_address on parking (address);
 create index if not exists parking_year on parking (year);
+
+alter table parking cluster on parking_year;
+
+vacuum verbose analyze;
--- sql/views/blocks.sql
+++ sql/views/blocks.sql
@@ -1,5 +1,5 @@
 create table if not exists blocks as
-  SELECT DISTINCT ON (geocoded_address)
+  select distinct on (geocoded_address)
     w.ward as ward,
     g.geocoded_address as address,
     g.geocoded_lng as lng,
@@ -11,11 +11,15 @@ create table if not exists blocks as
      when split_part(g.geocoded_address, ' ', 2) not in ('N', 'E', 'S', 'W') then null
      else split_part(g.geocoded_address, ' ', 2)
     end as cardinal_direction,
+		bg.tractce,
+		bg.geoid as bg_geoid,
     g.geom
   from
     geocodes g
   join
     wards2015 w on st_within(g.geom, w.wkb_geometry)
+	join tl_2016_17_bg bg on
+		st_within(g.geom, bg.wkb_geometry)
   where
       g.geocode_accuracy > 0.7 and
       g.geocoded_city = 'Chicago' and (
