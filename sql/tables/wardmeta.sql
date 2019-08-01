04603b0ae2aa2b9a863270d471d3a5bf168ed536
David Eads <davideads@gmail.com>
2019-07-31 21:59:14 -0500

    new ward meta for new city council

--- sql/tables/wardmeta.sql
+++ sql/tables/wardmeta.sql
@@ -1,10 +1,11 @@
 create table public.wardmeta (
-  ward character varying(2) primary key,
+  firstname character varying,
+  lastname character varying,
   alderman character varying,
-  address character varying,
-  city character varying,
-  state character varying,
-  zipcode character varying(5),
-  ward_phone character varying(14),
-  email character varying
+  ward character varying(2) primary key,
+  ward_phone character varying,
+  website_url character varying,
+  email character varying,
+  cq character varying,
+  notes character varying
 );
